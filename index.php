b36ebcd7039b6b28d9d45e15395cfe8698e47b7e
deathordie <deathordie@windowslive.com>
2016-05-27 18:05:14 +0700

    27-05-2016

--- index.php
+++ index.php
@@ -1,15 +1,42 @@
 <?php
     if(isset($_POST['Upload'])){
-        If($_FILES['file']['tmp_name']){
+        $checktype ='';
+         for($i=0; $i<count($_FILES['file']['name']);$i++){
+             
+            If($_FILES['file']['type'][$i] != 'image/jpeg'){
+                $checktype = FALSE;
+                
+                }
+            else{
+                
+                if($checktype == false)
+                    $checktype = FALSE;        
+                else
+                    $checktype = true;
+            }
+        }
+        if($checktype == 0)
+            echo "<script lang='javascript'>alert('กรุณาเลือกไฟล์รูปภาพเท่านั้น');</script>";
+        else
+        echo $checktype;
+    }
+        /*If($_FILES['file']['type'] == 'image/jpeg'){
             date_default_timezone_set('asia/bangkok');
-            $str = "Pic".date('dmY H-i-s');
+            $str = "Pic".date('d-m-Y H-i-s');
             //mkdir($str);
-            //move_uploaded_file($_FILES['file']['tmp_name'],'/'.$str);
-            echo $_FILES['file']['name'];
+            for($i=0;$i<= count($_FILES['file']['name']);$i++){
+               move_uploaded_file($_FILES['file']['name'][$i],'/'.$str);
+            }
             
         }
-    }
+        else
+        echo "<script lang='javascript'>alert('กรุณาเลือกไฟล์รูปภาไเท่านั้น');</script>";
+         
+         */
+    
+    
 ?>
+
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -22,9 +49,9 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <form method="post" enctype="multipart/form-data">
+        <form name="upload" method="post" enctype="multipart/form-data" >
             <table align="center">
-                <tr><td>ไฟล์</td><td><input type="file" name="file" multiple=""></td></tr>
+                <tr><td>ไฟล์</td><td><input type="file" name="file[]" multiple="multiple"></td></tr>
                 <tr><td colspan="2"><input type="submit" name="Upload" value="Upload"></td></tr>
             </table>
         </form>
