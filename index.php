466d6a1552a9151225d5c8d73f7196375a3faecd
deathordie <deathordie@windowslive.com>
2016-05-30 17:18:15 +0700

    30-5-2016

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     if(isset($_POST['Upload'])){
         $checktype = false;
          for($i=0; $i<count($_FILES['file']['name']);$i++){
-            If($_FILES['file']['type'][$i] == 'image/jpeg'){
+            If($_FILES['file']['type'][$i] == 'image/jpeg' || $_FILES['file']['type'][$i] == 'image/bmp' || $_FILES['file']['type'][$i] == 'image/png' || $_FILES['file']['type'][$i] == 'image/jpg'){
                 $checktype = true;
             }
             else{
@@ -13,39 +13,16 @@
          if($checktype == true){
             date_default_timezone_set('asia/bangkok');
             $str = "Pic".date('d-m-Y H-i-s');
-            mkdir($str);
+            $des = '//172.16.2.55/drive-g/HBAY/Pic/'.$str;
+            echo $des;
+            mkdir($des,0777);
             for($i=0; $i<count($_FILES['file']['name']);$i++){
-                move_uploaded_file($_FILES['file']['tmp_name'][$i],$str.'/'.$i.'.jpg');
-                chmod($str, 0775);
-                
+                move_uploaded_file($_FILES['file']['tmp_name'][$i],$des.'/Pic'.$i.'.jpg');
             }
-           
-            //move_uploaded_file($_FILES['file']['tmp_name'][$i],$str.'/'.$_FILES['file']['name'][$i]);
-            //echo "<script lang='javascript'>alert('บันทึกข้อมูลเรียบร้อยแล้ว');</script>";
-         }
+        }
          else
             echo "<script lang='javascript'>alert('กรุณาเลือกไฟล์รูปภาพเท่านั้น');</script>";
     }
-        //if($checktype == 0)
-          //  echo "<script lang='javascript'>alert('กรุณาเลือกไฟล์รูปภาพเท่านั้น');</script>";
-        //else
-        //echo $checktype;
-    
-        /*If($_FILES['file']['type'] == 'image/jpeg'){
-            date_default_timezone_set('asia/bangkok');
-            $str = "Pic".date('d-m-Y H-i-s');
-            //mkdir($str);
-            for($i=0;$i<= count($_FILES['file']['name']);$i++){
-               move_uploaded_file($_FILES['file']['name'][$i],'/'.$str);
-            }
-            
-        }
-        else
-        echo "<script lang='javascript'>alert('กรุณาเลือกไฟล์รูปภาไเท่านั้น');</script>";
-         
-         */
-    
-    
 ?>
 
 <!DOCTYPE html>
@@ -62,6 +39,7 @@ and open the template in the editor.
     <body>
         <form name="upload" method="post" enctype="multipart/form-data" >
             <table align="center">
+                <tr><td colspan="2"><label align="center"><h2>ระบบอัพโหลดรูปภาพ Hbay</h2></label></td><tr>
                 <tr><td>ไฟล์</td><td><input type="file" name="file[]" multiple="multiple"></td></tr>
                 <tr><td colspan="2"><input type="submit" name="Upload" value="Upload"></td></tr>
             </table>
